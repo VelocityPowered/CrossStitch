756e3b468afc633f8e1f9f11b1d4289540064d8e
Andrew Steinborn <git@steinborn.me>
2020-11-22 04:41:51 -0500

    Switch to DedicatedServerModInitializer

--- src/main/java/com/velocitypowered/crossstitch/CrossStitch.java
+++ src/main/java/com/velocitypowered/crossstitch/CrossStitch.java
@@ -1,14 +1,14 @@
 package com.velocitypowered.crossstitch;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CrossStitch implements ModInitializer {
+public class CrossStitch implements DedicatedServerModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(CrossStitch.class);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         LOGGER.info("CrossStitch at your service, binding together your mods and your proxy!");
     }
 }
