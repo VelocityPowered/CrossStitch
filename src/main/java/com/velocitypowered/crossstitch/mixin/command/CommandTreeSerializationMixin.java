440c6195be8f8ab6d71de7db20f699cba6bdcdc9
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-06-11 13:23:54 -0400

    Update mod to Minecraft 1.19

--- src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
+++ src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
@@ -3,41 +3,43 @@ package com.velocitypowered.crossstitch.mixin.command;
 import com.mojang.brigadier.arguments.ArgumentType;
 import io.netty.buffer.Unpooled;
 import net.minecraft.command.argument.ArgumentTypes;
+import net.minecraft.command.argument.serialize.ArgumentSerializer;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.CommandTreeS2CPacket;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(CommandTreeS2CPacket.class)
+@Mixin(CommandTreeS2CPacket.ArgumentNode.class)
 public class CommandTreeSerializationMixin {
-    private static final Identifier MOD_ARGUMENT_INDICATOR = new Identifier("crossstitch:mod_argument");
+    private static final int MOD_ARGUMENT_INDICATOR = -256;
 
-    @Redirect(method = "writeNode", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/argument/ArgumentTypes;toPacket(Lnet/minecraft/network/PacketByteBuf;Lcom/mojang/brigadier/arguments/ArgumentType;)V"))
-    private static void writeNode$wrapInVelocityModArgument(PacketByteBuf packetByteBuf, ArgumentType<?> type) {
-        ArgumentTypes.Entry entry = ArgumentTypes.classMap.get(type.getClass());
-        if (entry == null) {
-            packetByteBuf.writeIdentifier(new Identifier(""));
+    @Redirect(method = "write(Lnet/minecraft/network/PacketByteBuf;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/CommandTreeS2CPacket$ArgumentNode;write(Lnet/minecraft/network/PacketByteBuf;Lnet/minecraft/command/argument/serialize/ArgumentSerializer$ArgumentTypeProperties;)V"))
+    private <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void writeNode$wrapInVelocityModArgument(PacketByteBuf packetByteBuf, ArgumentSerializer.ArgumentTypeProperties<A> properties) {
+        Identifier id = Registry.COMMAND_ARGUMENT_TYPE.getId(properties.getSerializer());
+        if (id == null) {
+            packetByteBuf.writeVarInt(-1);
             return;
         }
-        if (entry.id.getNamespace().equals("minecraft") || entry.id.getNamespace().equals("brigadier")) {
-            packetByteBuf.writeIdentifier(entry.id);
-            entry.serializer.toPacket(type, packetByteBuf);
+        if (id.getNamespace().equals("minecraft") || id.getNamespace().equals("brigadier")) {
+            CommandTreeS2CPacket.ArgumentNode.write(packetByteBuf, properties);
             return;
         }
 
         // Not a standard Minecraft argument type - so we need to wrap it
-        serializeWrappedArgumentType(packetByteBuf, type, entry);
+        serializeWrappedArgumentType(packetByteBuf, id, properties.getSerializer(), properties);
     }
 
-    private static void serializeWrappedArgumentType(PacketByteBuf packetByteBuf, ArgumentType argumentType, ArgumentTypes.Entry entry) {
-        packetByteBuf.writeIdentifier(MOD_ARGUMENT_INDICATOR);
+    @SuppressWarnings("unchecked")
+    private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void serializeWrappedArgumentType(PacketByteBuf packetByteBuf, Identifier id, ArgumentSerializer<A, T> serializer, ArgumentSerializer.ArgumentTypeProperties<A> properties) {
+        packetByteBuf.writeVarInt(MOD_ARGUMENT_INDICATOR);
 
-        packetByteBuf.writeIdentifier(entry.id);
+        packetByteBuf.writeVarInt(Registry.COMMAND_ARGUMENT_TYPE.getRawId(properties.getSerializer()));
 
         PacketByteBuf extraData = new PacketByteBuf(Unpooled.buffer());
-        entry.serializer.toPacket(argumentType, extraData);
+        serializer.writePacket((T) properties, packetByteBuf);
 
         packetByteBuf.writeVarInt(extraData.readableBytes());
         packetByteBuf.writeBytes(extraData);
